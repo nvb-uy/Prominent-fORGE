f3cee2780cf161573a8acc908ec10988a3cbfa6f
nvb-uy <almhak@proton.me>
2023-05-16 05:28:43 -0300

    fix(serverside crash)

--- src/main/java/elocindev/prominent/forge/config/ConfigBuilder.java
+++ src/main/java/elocindev/prominent/forge/config/ConfigBuilder.java
@@ -3,19 +3,17 @@ package elocindev.prominent.forge.config;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.loading.FMLPaths;
 
 
 public class ConfigBuilder {
     public static final Gson BUILDER = (new GsonBuilder()).setPrettyPrinting().create();
   
-    public static final Path file = Paths.get(Minecraft.getInstance().gameDirectory.getAbsolutePath(), "config")
-    .resolve("prominent.json");
+    public static final Path file = FMLPaths.GAMEDIR.get().toAbsolutePath().resolve("config").resolve("prominent.json");
     
     public static ConfigEntries loadConfig() {
       try {
